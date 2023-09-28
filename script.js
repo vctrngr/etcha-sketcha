c9e665f712b9bf7a0566ecbfe2b219f145cedec0
vctrngr <ctorv.ictor@anche.no>
2023-09-28 18:52:24 +0100

    Make grid

--- script.js
+++ script.js
@@ -4,12 +4,30 @@ let gridSize = 16;
 
 const totalWidth = 960;
 
-container.style = `display: flex; width: ${totalWidth}px`;
 
-const sqr = document.createElement('div');
-sqr.style.border = 'solid black 1px';
-sqr.style.width = `${totalWidth / gridSize}px`
+function makeGrid(size){
 
-// make the grid in the specified grid size
+    if (size > 100){
+        size = 100;
+    }
 
-container.appendChild(sqr);
+    let borders = 1;
+
+    let realWidth = totalWidth + (borders * (2 * size));
+
+    container.style = `display: flex; width: ${realWidth}px; height: ${realWidth}px; flex-wrap: wrap; padding: 8px; margin: 10px; gap: 0`;
+
+   
+    for (let i = 0; i < size * size; i++){
+        const sqr = document.createElement('div');
+        sqr.style = 'margin: 0; padding: 0;';
+        sqr.style.border = `solid #666666 ${borders}px`;
+        sqr.style.width = `${totalWidth / size}px`;
+        sqr.style.height = `${totalWidth / size}px`;
+        // sqr.style.flex = '0 2 0';
+        container.appendChild(sqr);
+    }
+}
+
+makeGrid(56);
+
